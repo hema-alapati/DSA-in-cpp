fdd1f05a8cb9b77715801185993c1492b2f88d1b
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-02-10 19:38:47 +0530

    Add files via upload

--- 1PascalsTriangle.cpp
+++ 1PascalsTriangle.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#include <vector>
 class Solution{
     public:
         vector<vector<int>> generate(int numRows){
@@ -8,25 +8,24 @@ class Solution{
             for(int i=0;i<numRows;i++){
                 r[i].resize(i+1);
                 r[i][0]=r[i][i]=1;
-                for(int j=1;j<i;j++)}{
+                for(int j=1;j<i;j++){
                     r[i][j]=r[i-1][j-1]+r[i-1][j];
                 }
             }
         return r;
-        };
+        }
+};
 
-// int main() {
-//     Solution s;
-//     vector<vector<int>> ans=s.generate(5);
-//     for (int i = 0; i < ans.size(); i++)  
-//     { 
-//         for (int j = 0; j < ans[i].size(); j++)  
-//         {  
-//             cout << ans[i][j] << " ";  
-//         }     
-//         cout << endl;  
-//     }  
-//     return 0;
-// }
-
-//main function d
+int main() {
+    Solution s;
+    vector<vector<int>> ans=s.generate(5);
+    for (int i = 0; i < ans.size(); i++)  
+    { 
+        for (int j = 0; j < ans[i].size(); j++)  
+        {  
+            cout << ans[i][j] << " ";  
+        }     
+        cout << endl;  
+    }  
+    return 0;
+}
