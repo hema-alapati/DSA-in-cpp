607fbcdbea28a474da162cc0ffd93c99f342a06d
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-02-22 21:57:03 +0530

    Add files via upload

---
+++ 11Add2numbersWithout+.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    for(int i=1;i<=b;i++){
+        a++;
+    }
+    cout<<a<<endl;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter the values of a and b";
+    cin>>a>>b;
+    while(b!=0){
+        a++;b--;
+    }
+    cout<<a<<endl;
+}
+//using bit shifting
+#include <bits/stdc++.h>
+int main(){
+    int a,b;
+    cin>>a>>b;
+    //a stores sum, b stores carry
+    while(b!=0){
+        unsigned carry=a&b;
+        a=a^b;
+        b=carry<<1;
+    }
+    return 0;
+}
