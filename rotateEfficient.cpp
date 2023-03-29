4cac667ea4ee917589833d74857381ec5156cfe1
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-03-29 23:30:44 +0530

    Add files via upload

--- rotateEfficient.cpp
+++ rotateEfficient.cpp
@@ -1,44 +1,46 @@
-// C++ implementation of right rotation
-// of an array K number of times
-#include<bits/stdc++.h>
+// C++ program to rotate right an array by K times
+// The approach is simple yet optimized.
+//  The idea is to reverse the array three times. 
+// For the first time we reverse only the last k elements. 
+// Second time we will reverse first n-k(n=size of array) elements. 
+// Finally we will get our rotated array by reversing the entire array.
+
+#include <iostream>
 using namespace std;
-
-// Function to rightRotate array
-void RightRotate(int a[], int n, int k)
-{
-	
-	// If rotation is greater
-	// than size of array
-	k = k % n;
-
-	for(int i = 0; i < n; i++)
-	{
-	if(i < k)
-	{
-			
-		// Printing rightmost
-		// kth elements
-		cout << a[n + i - k] << " ";
-	}
-	else
-	{
-			
-		// Prints array after
-		// 'k' elements
-		cout << (a[i - k]) << " ";
-	}
-	}
-	cout << "\n";
-}
-	
-// Driver code
 int main()
 {
-	int Array[] = { 1, 2, 3, 4, 5,6,7 };
-	int N = sizeof(Array) / sizeof(Array[0]);
-	int K = 4;
-	
-	RightRotate(Array, N, K);
+	int arr[] = { 1, 3, 5, 7, 9, 11 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int k = 3; //No. of rotations
+	k = k % n;
+	int i, j;
+	// Reverse last k numbers
+	for (i = n - k, j = n - 1; i < j; i++, j--) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	// Reverse the first n-k terms
+	for (i = 0, j = n - k - 1; i < j; i++, j--) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	// Reverse the entire array
+	for (i = 0, j = n - 1; i < j; i++, j--) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+	// Print the rotated array
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+
+	return 0;
 }
 
-// This code is contributed by Surendra_Gangwar
+
+//this code is taken from gfg
+// https://www.geeksforgeeks.org/print-array-after-it-is-right-rotated-k-times/
