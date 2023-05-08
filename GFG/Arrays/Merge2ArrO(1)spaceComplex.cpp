4161eff9cdac7b84e67e7850db7f0cf36df07340
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-05-09 00:54:52 +0530

    Create Merge2ArrO(1)spaceComplex.cpp

---
+++ GFG/Arrays/Merge2ArrO(1)spaceComplex.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        {
+            int gap=ceil((float)(m+n)/2);
+            while(gap>0){
+                int i=0,j=gap;
+                
+                while(j<(n+m))
+                {
+                    if(j<n && arr1[i]>arr1[j])
+                    {
+                        swap(arr1[i],arr1[j]);
+                    }
+                    else if(j>=n && i<n && arr1[i]>arr2[j-n])
+                    {
+                        swap(arr1[i],arr2[j-n]);
+                    }
+                    else if(j>=n && i>=n && arr2[i-n]>arr2[j-n])
+                    {
+                        swap(arr2[i-n],arr2[j-n]);
+                    }
+                    
+                    i++;
+                    j++;
+                }
+                
+                if(gap==1)
+                {
+                    gap=0;
+                }
+                else
+                {
+                    gap=ceil((float) gap/2);
+                }
+            }
+        } 
+};
+
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int n,m;
+        cin>>n>>m;
+        long long arr1[n],arr2[m];
+        for(int i=0;i<n;i++){
+            cin>>arr1[i];
+        }
+        for(int i=0;i<m;i++){
+            cin>>arr2[i];
+        }
+        Solution ob;
+        ob.merge(arr1,arr2,n,m);
+        for(int i=0;i<n;i++){
+            cout<<arr1[i]<<" ";
+        }
+        cout<<endl;
+        for(int i=0;i<m;i++){
+            cout<<arr2[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
