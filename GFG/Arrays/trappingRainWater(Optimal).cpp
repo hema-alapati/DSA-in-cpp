6acd87f34243b43d9dc144695b749dd59662f5c7
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-05-31 18:34:20 +0530

    Update trappingRainWater(Optimal).cpp
    Updated intuition

--- GFG/Arrays/trappingRainWater(Optimal).cpp
+++ GFG/Arrays/trappingRainWater(Optimal).cpp
@@ -1,9 +1,11 @@
-//https://practice.geeksforgeeks.org/problems/trapping-rain-water-1587115621/1
-//this is the optimal solution and it takes o(n) time complexity. The brute force solution taking o(n^2) tc is yet to be implemented. 
+//https://practice.geeksforgeeks.org/problems/trapping-rain-water-1587115621/1The brute force solution taking o(n^2) tc is yet to be implemented. 
 //logic for brute force solution:
 //iterate the array, for every element i, use min(left max, right max) - height(or array)[i]
-
-
+//intuition for optimal:
+//in bruteforce we are taking min(leftmax,rightmax)-arr[i]
+//but here we are either taking leftmax or rightmax. how it is working is: we are taking condition if arr[left]<=arr[right](line 13) 
+//from that we can make sure that there is atleast one element on the right which is greater or equal to this, which means we can store water
+//on this block equals to leftmax-it's height. because right cannot be lower than this. 
 
 long long trappingWater(int arr[], int n){
         int left=0,right=n-1;
@@ -24,8 +26,4 @@ long long trappingWater(int arr[], int n){
         return ans;
     }
 
-//note/intuition:
-//in bruteforce we are taking min(leftmax,rightmax)-arr[i]
-//but here we are either taking leftmax or rightmax. how it is working is: we are taking condition if arr[left]<=arr[right](line 13) 
-//from that we can make sure that there is atleast one element on the right which is greater or equal to this, which means we can store water
-//on this block equals to leftmax-it's height. because right cannot be lower than this. 
+
